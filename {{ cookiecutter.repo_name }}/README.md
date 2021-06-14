93b700dc14f4aa4fb86416d25314c7408a9039d8
Bouke Postma <bouke.postma@adaptfy.com>
2021-06-14 14:53:03 +0200

    added some items from vincent

--- {{ cookiecutter.repo_name }}/README.md
+++ {{ cookiecutter.repo_name }}/README.md
@@ -56,5 +56,9 @@ Project Organization
 # Introduction 
 TODO: Give a short introduction of your project. Let this section explain the objectives or the motivation behind this project. 
 
-# Build and Test
-TODO: Describe and show how to build your code and run the tests. 
+## Installation
+Install `{{ cookiecutter.python_module_name }}` via:
+
+```bash
+$ make develop
+```
--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,9 +1,26 @@
+import os
 from setuptools import find_packages, setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+base_packages = ["numpy>=1.20.3", "scipy>=1.6.3", "scikit-learn>=0.24.2",
+                 "pandas>=1.2.4", "plotly-express>=0.4.1", "jupyter>=1.0.0", 
+                 "tqdm>=4.61.1", "click>=8.0", "fastapi>=0.65.2", 'Sphinx']
+
+dev_packages = ["pip", "pytest-cov", "pytest", "flake8", "mkdocs>=1.2.1", 
+                "mkdocs-material>=7.1.8", "pre-commit>=2.13.0", "mkdocs-jupyter==0.17.3",
+                "coverage", "awscli"]
 
 setup(
     name='src',
-    packages=find_packages(),
-    version='0.1.0',
+    version='0.0.1',
+    packages=find_packages(exclude=['data', 'notebooks']),
     description='{{ cookiecutter.description }}',
+    long_description=read('readme.md'),
+    install_requires=base_packages,
     author='{{ cookiecutter.author_name }}',
+    extras_require={
+        "dev": dev_packages
+    }
 )

259932ab5b6b089912931cae2c9e4344a3e9b06f
Bouke Postma <bouke.postma@adaptfy.com>
2021-06-14 10:28:35 +0200

    added logger

---
+++ {{ cookiecutter.repo_name }}/logger.py
@@ -0,0 +1,19 @@
+import logging
+from rich.logging import RichHandler
+
+logger = logging.getLogger(__name__)
+
+# the handler determines where the logs go: stdout/file
+shell_handler = RichHandler()
+
+# the formatter determines what our logs will look like
+fmt_shell = '%(levelname)s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s'
+formatter = logging.Formatter(fmt_shell)
+shell_handler.setFormatter(formatter)
+
+logger.addHandler(shell_handler)
+
+# FYI: Levels of logging go: debug, info, warning, critical, error
+logger.setLevel(logging.DEBUG)
+shell_handler.setLevel(logging.DEBUG)
+

ce2b806958406d93e2ed5bb7ab1ef33e62c46563
Bouke Postma <bouke.postma@adaptfy.com>
2021-06-15 12:16:15 +0200

    example docstring

--- {{ cookiecutter.repo_name }}/main.py
+++ {{ cookiecutter.repo_name }}/main.py
@@ -1,10 +1,21 @@
 """
-Main script of the project to run the entire pipeline
+Main script of the project to run the entire pipeline. Example of docstring in Google style.
 """
 
 
 def main():
-    pass
+    """Summary line.
+
+    Extended description of function.
+
+    Args:
+        arg1 (int): Description of arg1
+        arg2 (str): Description of arg2
+
+    Returns:
+        bool: Description of return value
+    """
+    return True
 
 
 if __name__ == "main":

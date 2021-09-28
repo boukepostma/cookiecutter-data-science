fffd661f8780541da895f07310e3770099d88b8f
Bouke Postma <bouke.postma@adaptfy.com>
2021-09-28 10:13:28 +0200

    added logging to file

--- {{ cookiecutter.repo_name }}/logger.py
+++ {{ cookiecutter.repo_name }}/logger.py
@@ -1,8 +1,9 @@
-""" 
+"""
 See this short series of videos for an introduction on logging: https://calmcode.io/logging/introduction.html
 """
 
 import logging
+import os
 
 from rich.logging import RichHandler
 
@@ -10,16 +11,18 @@ logger = logging.getLogger(__name__)
 
 # the handler determines where the logs go: stdout/file
 shell_handler = RichHandler()
+file_handler = logging.FileHandler(filename=os.sep.join(["logs", "logs.log"]))
 
 # the formatter determines what our logs will look like
-fmt_shell = (
-    "%(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s"
-)
+fmt_shell = "%(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s"
 formatter = logging.Formatter(fmt_shell)
 shell_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
 
 logger.addHandler(shell_handler)
+logger.addHandler(file_handler)
 
 # FYI: Levels of logging go: debug, info, warning, critical, error
 logger.setLevel(logging.DEBUG)
-shell_handler.setLevel(logging.DEBUG)
+shell_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)

6088dcebdc8509acbcd9f026eebb565b1e613cf3
Bouke Postma <bouke.postma@adaptfy.com>
2022-02-14 18:27:48 +0100

    no more package versions bc too dififcult to maintain

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -8,35 +8,25 @@ def read(fname):
 
 # Here all dependencies that are required to run the package should be included  
 base_packages = [
-    "numpy>=1.20.3",
-    "scipy>=1.6.3",
-    "scikit-learn>=0.24.2",
-    "pandas>=1.2.4",
-    "plotly-express>=0.4.1",
-    "jupyter>=1.0.0",
-    "tqdm>=4.61.1",
+    "numpy",
+    "scipy",
+    "scikit-learn",
+    "pandas",
+    "plotly-express",
+    "tqdm",
     "rich",
     "openpyxl",
-    "ipython>=7.27.0",
-    "ipykernel>=6.4.1",
-    "sktime==0.8.0",
-    "pmdarima",
-    "seaborn",
-    {%- if cookiecutter.prophet == "True" %}
-    "pystan==2.19.1.1",
-    "fbprophet==0.7.1", 
-    {%- endif %}
 ] 
 
 # Here all dependencies that are required to develop the package should be included
 dev_packages = [
+    "jupyter",
+    "ipython",
+    "ipykernel",
     "pip",
-    "pytest-cov",
     "pytest",
     "flake8",
-    "pre-commit>=2.13.0",
-    "coverage",
-    "awscli",
+    "pre-commit",
     "black",
     "isort",
 ]

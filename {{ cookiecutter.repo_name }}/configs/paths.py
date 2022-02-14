234390e33e21bf1b99877e02fc4d527a1f0b857e
Bouke Postma <bouke.postma@adaptfy.com>
2022-02-14 18:03:32 +0100

    no rich logger when using notebooks

--- {{ cookiecutter.repo_name }}/configs/paths.py
+++ {{ cookiecutter.repo_name }}/configs/paths.py
@@ -15,6 +15,7 @@ FOLDER_NOTEBOOKS = FOLDER_ROOT / "notebooks"
 FOLDER_REPORTS = FOLDER_ROOT / "reports"
 FOLDER_REFERENCES = FOLDER_ROOT / "references"
 FOLDER_DOCS = FOLDER_ROOT / "docs"
+FOLDER_LOGS = FOLDER_ROOT / "logs"
 
 FOLDER_DATA_RAW = FOLDER_DATA / "raw"
 FOLDER_DATA_EXTERNAL = FOLDER_DATA / "external"
@@ -23,7 +24,11 @@ FOLDER_DATA_PROCESSED = FOLDER_DATA / "processed"
 
 FOLDER_FIGURES = FOLDER_REPORTS / "figures"
 
+# LOGGER FILE PATH
+FILE_LOGGER = FOLDER_LOGS / "logs.log"
+
 # FILE PATHS: Paths to files (.csv, .xlsx), databases (.db, .sqlite) and their respective table names
+
 # Raw data
 FILE_PATH_EXAMPLE = FOLDER_DATA_RAW / "example.xlsx"
 
--- {{ cookiecutter.repo_name }}/logger.py
+++ {{ cookiecutter.repo_name }}/logger.py
@@ -12,22 +12,43 @@ import os
 
 from rich.logging import RichHandler
 
+from configs.paths import FILE_LOGGER
+
 logger = logging.getLogger(__name__)
 
-# the handler determines where the logs go: stdout/file
-shell_handler = RichHandler()
-file_handler = logging.FileHandler(filename=os.sep.join(["logs", "logs.log"]))
+try:
+    # This will evoke an error when using ipython (e.g. notebooks)
+    get_ipython()  # noqa
+    
+    # Use regular logger without rich or filehandler when using ipython (notebooks)
+    shell_handler = logging.StreamHandler()
 
-# the formatter determines what our logs will look like
-fmt_shell = "%(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s"
-formatter = logging.Formatter(fmt_shell)
-shell_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
+    # the formatter determines what our logs will look like
+    fmt_shell = "%(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s"
+    formatter = logging.Formatter(fmt_shell)
+    shell_handler.setFormatter(formatter)
 
-logger.addHandler(shell_handler)
-logger.addHandler(file_handler)
+    logger.addHandler(shell_handler)
 
-# FYI: Levels of logging go: debug, info, warning, critical, error
-logger.setLevel(logging.DEBUG)
-shell_handler.setLevel(logging.INFO)
-file_handler.setLevel(logging.DEBUG)
+    # FYI: Levels of logging go: debug, info, warning, critical, error
+    logger.setLevel(logging.INFO)
+    shell_handler.setLevel(logging.INFO)
+
+except NameError:
+    # the handler determines where the logs go: stdout/file
+    shell_handler = RichHandler()
+    file_handler = logging.FileHandler(filename=FILE_LOGGER)
+
+    # the formatter determines what our logs will look like
+    fmt_shell = "%(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s"
+    formatter = logging.Formatter(fmt_shell)
+    shell_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(shell_handler)
+    logger.addHandler(file_handler)
+
+    # FYI: Levels of logging go: debug, info, warning, critical, error
+    logger.setLevel(logging.DEBUG)
+    shell_handler.setLevel(logging.DEBUG)
+    file_handler.setLevel(logging.DEBUG)
